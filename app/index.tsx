1a146d03b4f11e70090faea7f456ad3d3e630d32
rozer <shubhamsinha07084@gmail.com>
2025-11-19 14:23:38 +0530

    fetching image

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 
 interface Pokemon {
   name: string;
-  url: string;  
+  image: string;  
 }
 
 export default function Index() {
@@ -20,9 +20,30 @@ export default function Index() {
 
   async function fetchPokemons() {
     try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
       const data = await response.json();
-      setPokemons(data.results);
+
+      // fetch detailed info for each pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          try {
+            const res = await fetch(pokemon.url);
+
+            const details = await res.json();
+            if (!details.sprites?.front_default) return null;
+
+            return {
+              name: pokemon.name,
+              image: details.sprites.front_default,
+            };
+          } catch (error) {
+            return null;
+          }
+        })
+      );
+
+      // remove null entries and set pokemons
+      setPokemons(detailedPokemons.filter(Boolean) as Pokemon[]);
 
     } catch (error) {
       console.log(error)
@@ -30,10 +51,11 @@ export default function Index() {
   }
 
   return (
-    <ScrollView style={{ padding: 20, backgroundColor: '#dc2222ff' }}>
+    <ScrollView style={{ padding: 20, }}>
      {pokemons.map((pokemon) => (
       <View key={pokemon.name} style={{ marginBottom: 10, padding: 10, backgroundColor: '#fff', borderRadius: 5 }}  >
         <Text>{pokemon.name}</Text>
+          <Image source={{ uri: pokemon.image }} style={{ width: 100, height: 100 }} />
       </View>
      ))}
     </ScrollView>
